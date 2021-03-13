8c2ebeedf12ccca02ad8b2b1dec60d7b99ad07b3
Zsolt Szakal <zsltszakal@gmail.com>
2021-03-13 18:23:16 +0000

    Blog Article

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default () => {
+  return (
+    <>
+      Hello world!
+      <p><Link to="/blog">View Blog</Link></p>
+    </>)
+}
